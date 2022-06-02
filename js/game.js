b62351457b4c68444c230f832a59d04b32ee8a4c
Anita Kay <99274242+anitak456@users.noreply.github.com>
2022-06-02 14:50:50 +0000

    Added comments to our files

--- js/game.js
+++ js/game.js
@@ -1,10 +1,30 @@
 /* global Phaser */
 
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Anita Kay
+// Created on: June 2022
+// Modified by: Anita Kay
+// This is the Phaser3 configuration file
+
+//* Setup for game scene */
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-  backgroundColor: 0x5f6e7a
+  physics: {
+    default: 'arcade',
+    arcade:{
+      debug: true
+    }
+  },
+  // Set background color
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // We place it in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
 }
 
 const game = new Phaser.Game(config)

ad6f6f774dcb98ca0cac4641ff87f1729991cfdf
Anita Kay <99274242+anitak456@users.noreply.github.com>
2022-06-12 19:11:55 +0000

    Updated gameScene.js

--- js/gameScene.js
+++ js/gameScene.js
@@ -13,6 +13,7 @@ class GameScene extends Phaser.Scene {
 
     this.background = null
     this.fairy = null
+    this.fireSparkles = false
   }
 
   //Initialize to activate scene
@@ -27,7 +28,9 @@ class GameScene extends Phaser.Scene {
     //Load images
     this.load.image('forestBackground', 'images/forestBackground.jpg')
     this.load.image('fairy', 'images/fairy.png')
-    
+    this.load.image('sparkles', 'images/sparkles.png')
+    //Load sound files
+    this.load.audio('twinkle', 'sounds/twinkle.wav')
   }
 
   // Show the images to the user and center them
@@ -37,6 +40,9 @@ class GameScene extends Phaser.Scene {
 
     // Show the sprite
     this.fairy = this.physics.add.sprite(1920 / 2, 1080 - 200, 'fairy').setScale(0.7)
+
+    // Create a group for the sparkles
+    this.sparklesGroup = this.physics.add.group() 
   }
 
   // Update so that the sprite can move. Called 60 times a second.
@@ -44,20 +50,47 @@ class GameScene extends Phaser.Scene {
 
     const keyLeftObj = this.input.keyboard.addKey('LEFT')
     const keyRightObj = this.input.keyboard.addKey('RIGHT')
+    const keySpaceObj = this.input.keyboard.addKey('SPACE')
 
-    if (keyLeftObj.isDown == true) {
+    //Use an if statement so that the sprite can move left.
+    if (keyLeftObj.isDown === true) {
       this.fairy.x -= 15
       if (this.fairy.x < 0) {
+        this.fairy.x = 1920
+      }
+    }
+
+    //Use an if statement so that the sprite can move right.
+    if (keyRightObj.isDown === true) {
+      this.fairy.x += 15
+      if (this.fairy.x > 1920) {
         this.fairy.x = 0
       }
     }
 
-    if(keyRightObj.isDown == true) {
-      this.fairy.x += 15
-      if (this.fairy.x > 1920) {
-        this.fairy.x = 1920
+    //Use an if statement so that the sprite shoots sparkles.
+    if (keySpaceObj.isDown === true) {
+      if (this.fireSparkles === false) {
+        // Fire sparkles
+        this.fireSparkles = true
+        const aNewSparkles = this.physics.add.sprite(this.fairy.x, this.fairy.y, 'sparkles')
+        this.sparklesGroup.add(aNewSparkles)
+        //Add sound to sparkles
+        this.sound.play('twinkle')
       }
     }
+
+    if (keySpaceObj.isUp === true) {
+      this.fireSparkles = false
+    }
+    //Allow sparkles to move upwards.
+    this.sparklesGroup.children.each(function (item) {
+      item.y = item.y - 15
+      // Destroy sparkles once they have left the screen.
+      if (item.y < 0) {
+        item.destroy()
+      }
+    })
   }
 }
 
